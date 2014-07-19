edb024119f131df0af8b633a53c93edcdaefa873
Raynos <raynos2@gmail.com>
2014-07-18 21:28:19 -0700

    add more docs

---
+++ docs.mli
@@ -0,0 +1,14 @@
+import { VElem, VThunk } from "vtree"
+
+vdom-thunk/immutable-thunk : (
+    fn: (...args: Any) => VElem,
+    args: Array<Any>,
+    key: String | null,
+    eqFn: (Array<Any>, Array<Any>) => Boolean
+) => VThunk
+
+vdom-thunk/partial : (
+    eq: (Array<Any>, Array<Any>) => Boolean
+) => (fn: Function<VElem>, ...args: Any) => VThunk
+
+vdom-thunk : (fn: Function<VElem>, ...args: Any) => VThunk
